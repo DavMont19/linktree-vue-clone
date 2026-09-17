41b8e6cec5ac948cf0dfbf7ba06495ecaf618611
DavMont19 <davmontoya6@gmail.com>
2026-09-17 16:39:44 -0500

    changes

--- src/components/Linkcard.vue
+++ src/components/Linkcard.vue
@@ -7,12 +7,12 @@
     >
     <!-- Icon Container-->  
 
-    <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-gray-700 text-green-400 group-hover: scale-110 group-hover:bg-green-500 group-hover:text-white group-hover:shadow-xl group-hover:shadow-green-500/20">
+    <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-gray-700 text-green-400 group-hover:scale-110 group-hover:bg-green-500 group-hover:text-white group-hover:shadow-xl group-hover:shadow-green-500/20">
         <svg 
             viewBox="0 0 24 24" 
             fill="none" 
             stroke="currentColor"
-            class=" h-6 w-6 transistion-transform duration-300 group-hover:rotate-6">
+            class=" h-6 w-6 transition-transform duration-300 group-hover:rotate-6">
         <path 
             stroke-linecap="round" 
             stroke-linejoin="round" 
@@ -29,6 +29,6 @@
 
      <!-- Hover Glow Effect-->
 
-     <div class="absolute inset-0 rounded-xl bg-linear-to-r from-green-500/0 via-green-500/10 to-green-500/0 opacity-0 transistion-opacity duration-300 group-hover:opacity-100"/>
+     <div class="absolute inset-0 rounded-xl bg-linear-to-r from-green-500/0 via-green-500/10 to-green-500/0 opacity-0 transition-opacity duration-300 group-hover:opacity-"/>
     </a>
 </template>
